b46cb0479e68b881bc30087aca14b0503ee19d3a
Kobe <kobearelativo@gmail.com>
2022-02-17 17:17:10 +0800

    Initial commit

---
+++ src/main/java/com/headhunter/inventory/HeadhunterApplication.java
@@ -0,0 +1,13 @@
+package com.headhunter.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadhunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadhunterApplication.class, args);
+	}
+
+}
