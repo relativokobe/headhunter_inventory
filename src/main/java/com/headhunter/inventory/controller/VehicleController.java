3b634dccb01dd577f6d7b7fe2c20463a7729baac
Kobe <kobearelativo@gmail.com>
2022-02-17 23:24:46 +0800

    Implement get all vehicles with sorting and pagination capabilities

--- src/main/java/com/headhunter/inventory/controller/VehicleController.java
+++ src/main/java/com/headhunter/inventory/controller/VehicleController.java
@@ -1,25 +1,32 @@
 package com.headhunter.inventory.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.headhunter.inventory.dto.VehicleDto;
 import com.headhunter.inventory.enums.ResponseEnum;
 import com.headhunter.inventory.model.ValidatorResponse;
 import com.headhunter.inventory.service.VehicleService;
 import com.headhunter.inventory.util.DtoEntityConverter;
 import com.headhunter.inventory.util.VehicleValidator;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class VehicleController {
     @Autowired
     private VehicleService vehicleService;
+
+    private static final int ZERO = 0;
+    private static final String SORT_BY_PARAM = "sortBy";
+    private static final String PAGE_START_PARAM = "pageStart";
+    private static final String PAGE_END_PARAM = "pageEnd";
 
     @PostMapping("/vehicle")
     public void insertNewVehicle(final @RequestBody VehicleDto vehicleDto, final HttpServletResponse response) throws IOException {
@@ -39,5 +46,30 @@ public class VehicleController {
 
         vehicleService.insertNewVehicle(DtoEntityConverter.convertVehicleDtoToEntity(vehicleDto));
         response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    @GetMapping("/vehicles")
+    public void getAllVehicles(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
+        final String sortBy = request.getParameter(SORT_BY_PARAM);
+        final String pageStartString = request.getParameter(PAGE_START_PARAM);
+        final String pageEndString = request.getParameter(PAGE_END_PARAM);
+
+        final ValidatorResponse validatorResponse = VehicleValidator.validateGetAllVehicles(sortBy, pageStartString, pageEndString);
+        if(!validatorResponse.getCode().equals(ResponseEnum.VALIDATION_SUCCESSFUL.getCode())){
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().write(validatorResponse.getDescription());
+            return;
+        }
+
+        final int pageStart = StringUtils.isEmpty(pageStartString) ? ZERO : Integer.parseInt(pageStartString);
+        final int pageEnd = StringUtils.isEmpty(pageEndString) ? ZERO : Integer.parseInt(pageEndString);
+
+        final List<VehicleDto> vehicles = DtoEntityConverter
+                .convertVehicleEntityToDtoList(vehicleService.getAllVehicles(pageStart, pageEnd, sortBy));
+
+        final Gson gson = new GsonBuilder().serializeNulls().create();
+        final String stringResponse = gson.toJson(vehicles);
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().write(stringResponse);
     }
 }
