559e08d2b85098e739887174e199110a5aedff78
Kobe <kobearelativo@gmail.com>
2022-02-17 23:17:48 +0800

    Use ModelMapper to map entity and dto

--- src/main/java/com/headhunter/inventory/util/DtoEntityConverter.java
+++ src/main/java/com/headhunter/inventory/util/DtoEntityConverter.java
@@ -2,15 +2,25 @@ package com.headhunter.inventory.util;
 
 import com.headhunter.inventory.dto.VehicleDto;
 import com.headhunter.inventory.entity.Vehicle;
+import org.modelmapper.ModelMapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class DtoEntityConverter {
-    public static Vehicle convertVehicleDtoToEntity(final VehicleDto vehicleDto){
-        final Vehicle vehicle = new Vehicle();
-        vehicle.setColor(vehicleDto.getColor());
-        vehicle.setInventoryCode(vehicleDto.getInventoryCode());
-        vehicle.setName(vehicleDto.getName());
-        vehicle.setModel(vehicleDto.getModel());
+    private static final ModelMapper modelMapper = new ModelMapper();
 
-        return vehicle;
+    public static Vehicle convertVehicleDtoToEntity(final VehicleDto vehicleDto){
+        return modelMapper.map(vehicleDto, Vehicle.class);
+    }
+
+    public static List<VehicleDto> convertVehicleEntityToDtoList(final List<Vehicle> vehicles){
+        return vehicles.stream()
+                .map(entity -> convertEntityToDto(entity))
+                .collect(Collectors.toList());
+    }
+
+    public static VehicleDto convertEntityToDto(final Vehicle vehicle){
+        return modelMapper.map(vehicle, VehicleDto.class);
     }
 }
